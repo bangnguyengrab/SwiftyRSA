d62f17b4c55d8d5a854ec87fd39dcd298d62a3b8
Sajal Gupta <sajal.gupta@grabtaxi.com>
2026-01-20 14:24:43 +0530

    Add Library Evolution support via unsafeFlags

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
         .target(
             name: "SwiftyRSA",
             dependencies: [],
-            path: "Source")
+            path: "Source",
+            swiftSettings: [
+                .unsafeFlags(["-Xfrontend", "-enable-library-evolution"])
+            ])
     ]
 )
