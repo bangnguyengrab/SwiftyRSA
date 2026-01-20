064cf636687bd019730d46f6d14d7f31968fb8df
Sajal Gupta <sajal.gupta@grabtaxi.com>
2026-01-20 14:15:30 +0530

    Enable Library Evolution in Package.swift using unsafeFlags (required for SPM)

--- Package.swift
+++ Package.swift
@@ -15,9 +15,8 @@ let package = Package(
             name: "SwiftyRSA",
             dependencies: [],
             path: "Source",
-            buildSettings: [
-                .init("BUILD_LIBRARY_FOR_DISTRIBUTION", to: "YES"),
-                .init("OTHER_SWIFT_FLAGS", to: "$(inherited) -Xfrontend -enable-library-evolution")
+            swiftSettings: [
+                .unsafeFlags(["-Xfrontend", "-enable-library-evolution"])
             ])
     ]
 )
